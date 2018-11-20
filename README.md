23c448099c766588cee70d216873585cf6ab8f65
tmcdos <tmcdos@abv.bg>
2018-11-20 21:02:57 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# CustomPatch
+
+Patch a buggy NPM package without forking it.
+
+Based on the handy [diff](https://github.com/kpdecker/jsdiff) package (there is a `jsdiff` published on the NPM registry but it is a bogus one)
+
+## WHY
+
+This tool does what [patch-package](https://www.npmjs.com/package/patch-package) does (or more accurately - should be doing) but without the related issues:
+
+- it works faster, especially when creating the patch
+- it never chokes on its own patches
+- it does not break if you try to apply a patch more than once (but shows a warning)
+- it does not require/depend on GIT
+ 
+When you have made a bugfix for one of your dependencies but the author/maintainers refuse to accept your PR - you do not need to fork the package.
+Create a patch and seamlessly apply it locally for all your builds.
+
+## Installation
+```bash
+npm install custompatch -g
+```
+
+## Setup
+
+It operates in exactly the same way as [patch-package](https://www.npmjs.com/package/patch-package) so you can use the same setup if you have it already.
+Add the following to your `package.json`
+
+```json
+"scripts": 
+{
+  "prepare": "custompatch"
+}
+```
+
+## Usage
+
+### Making patches
+
+Open your IDE and make the required changes for the bugfix inside your dependency in `node_modules`. After that run
+
+```bash
+custompatch "name-of-the-buggy-package"
+```
+
+This will create a folder called `patches` inside your project and a file called `name-of-the-buggy-package@version.patch` in this folder.
+This file will be a unified diff between your fixed version of the dependency and its original code.
+
+You can specify more than 1 package on the command-line - but no command-line options are accepted.
+
+### Updating patches
+
+The process is exactly the same as for creating a patch for the first time.
+
+### Applying patches
+
+Run `custompatch` without arguments inside your project folder - it will apply all the patches from `patches` folder.
+Currently there is no possibility to apply only the patch for a specific dependency. Also, it is not yet possible to undo a patch.
+Perhaps the command-line utility `patch` can do the job but this has not been tested:
+
+```bash
+patch -p1 -i patches/package-name@2.5.16.patch
+```
+
+## Benefits of patching over forking
+
+- Forks often require extra build steps - and even more often you do not need this overhead
+- CustomPatch warns you when a patch was not applied cleanly - so that you can check that your fix is still valid for the new version of dependency
+- Keep your patches colocated with the code that depends on them
+- Patches can be reviewed as part of your normal review process, forks probably can't
+
+## License
+
+Software License Agreement (BSD License)
+
+Copyright (c) 2018, IVO GELOV
+
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above
+  copyright notice, this list of conditions and the
+  following disclaimer.
+
+* Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+* Neither the name of IVO GELOV nor the names of its
+  contributors may be used to endorse or promote products
+  derived from this software without specific prior
+  written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ index.js
@@ -0,0 +1,292 @@
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const npmFolder = path.join(npmDir(),'node_modules','npm','node_modules');
+const pacote = require(path.join(npmFolder,'pacote'));
+const rimraf = require(path.join(npmFolder,'rimraf'));
+const diff = require('diff');
+
+const version = '1.0';
+// ANSI styles
+const ansiColors = ['black','red','green','yellow','blue','magenta','cyan','white'];
+
+function startColor(colorName, background)
+{
+  let idx = ansiColors.indexOf(colorName);
+  if(idx !== -1) return ansi(idx + (background ? 40 : 30));
+  idx = ansiColors.indexOf(colorName.replace('Bright',''));
+  if(idx !== -1) return ansi(idx + (background ? 100 : 90));
+  return ansi(background ? 100 : 90); // grey
+}
+
+function stopColor(background)
+{
+  return ansi(background ? 49 : 39);
+}
+
+function ansi(code)
+{
+  return '\u001B[' + code + 'm';
+}
+
+function echo()
+{
+  console.log.apply(null,arguments);
+}
+
+const curDir = process.cwd();
+const tmpDir = os.tmpdir();
+const patchDir = path.join(curDir, 'patches');
+
+echo(startColor('whiteBright') + 'CustomPatch' + stopColor() + ' version ' + startColor('greenBright') + version + stopColor());
+if(!fs.existsSync(path.join(curDir, '/node_modules')))
+{
+  echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Missing ' + startColor('whiteBright') + '"node_modules"' + stopColor() + ' folder');
+  process.exit(1);
+}
+
+//process.argv.shift(); // remove Node
+process.argv.shift(); // remove this script
+
+const len = process.argv.length;
+if(len > 0)
+{
+  // create patches
+  if(!fs.existsSync(patchDir)) fs.mkdirSync(patchDir);
+  for(let i = 0; i < len; i++)
+  {
+    makePatch(process.argv[i]);
+  }
+}
+else
+{
+  if(!fs.existsSync(patchDir))
+  {
+    echo(startColor('yellowBright') + 'WARNING: ' + stopColor() + 'Missing ' + startColor('whiteBright') + 'patches' + stopColor() + ' folder - nothing to do');
+    process.exit(2);
+  }
+  // apply patches
+  const patches = fs.readdirSync(patchDir);
+  patches.forEach(item =>
+  {
+    if(item.substr(-6) !== '.patch') return;
+    const pkg = item.replace('.patch','').split('@');
+    const dest = path.join(curDir, 'node_modules', pkg[0]);
+    if(!fs.existsSync(dest))
+    {
+      echo(startColor('yellowBright') + 'WARNING: ' + stopColor() + 'Package ' + startColor('whiteBright') + pkg[0] + stopColor() + ' is not installed - skipping this patch');
+      return;
+    }
+    readPatch(pkg[0], pkg[1]);
+  });
+}
+
+// find the NPM folder because PACOTE package is relative to this folder (not a global package)
+function npmDir()
+{
+  if(process.platform.indexOf('win') === 0)
+  {
+    return process.env.APPDATA ? path.join(process.env.APPDATA, 'npm') : path.dirname(process.execPath);
+  }
+  else
+  {
+    // /usr/local/bin/node --> prefix=/usr/local
+    let prefix = path.dirname(path.dirname(process.execPath));
+
+    // destdir is respected only on Unix
+    if (process.env.DESTDIR) prefix = path.join(process.env.DESTDIR, prefix);
+    return prefix;
+  }
+}
+
+// returns FALSE on error, Package.JSON on success
+function getConfig(pkgName)
+{
+  const folder = path.join(curDir, 'node_modules', pkgName);
+  const cfgName = path.join(folder, 'package.json');
+
+  if(!fs.existsSync(folder))
+  {
+    echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Missing folder "' + startColor('whiteBright') + './node_modules/' + stopColor() + startColor('greenBright') + pkgName + stopColor() + '"');
+    return false;
+  }
+  try
+  {
+    fs.accessSync(cfgName, fs.constants.R_OK);
+  }
+  catch (e)
+  {
+    echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Can not read ' + startColor('whiteBright') + '"package.json"' + stopColor());
+    return false;
+  }
+  const pkgConfig = fs.readFileSync(cfgName,'utf8');
+
+  let cfg = {};
+  try
+  {
+    cfg = JSON.parse(pkgConfig);
+  }
+  catch(e)
+  {
+    echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Could not parse ' + startColor('whiteBright') + '"package.json"' + stopColor() + ' - ' + startColor('redBright') + e.message + stopColor());
+    return false;
+  }
+  return cfg;
+}
+
+// build a patch for the given package
+function makePatch(pkgName)
+{
+  echo('Creating patch for: ' + startColor('magentaBright') + pkgName + stopColor());
+  const cfg = getConfig(pkgName);
+  if(cfg && cfg._resolved !== '') fetchPackage(pkgName, cfg._resolved, cfg.version, comparePackages);
+  else
+  {
+    echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Could not find the ' + startColor('whiteBright') + 'URL' + stopColor() + ' for ' + startColor('greenBright') + 'tarball' + stopColor());
+  }
+}
+
+// download the tarball
+function fetchPackage(pkgName, url, version, callback)
+{
+  echo('Fetching tarball of ' + startColor('whiteBright') + pkgName + stopColor() + ' from ' + startColor('green') + url + stopColor());
+  const dest = path.join(tmpDir, pkgName);
+  pacote.extract(url, dest).then(function ()
+  {
+    callback(pkgName, version);
+    rimraf(dest, function (err)
+    {
+      if(err) echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Could not clean up the TEMP folder');
+    });
+  }).catch(function (err)
+  {
+    echo(startColor('redBright') + err.message + stopColor());
+  });
+}
+
+// compare all files from the locally modified package with their original content
+function comparePackages(pkgName, version)
+{
+  const stream = fs.createWriteStream(path.join(patchDir, pkgName + '@' + version + '.patch'));
+  stream.cork();
+  scanFiles(pkgName, '', stream);
+  stream.end();
+  echo('Successfully created ' + startColor('greenBright') + pkgName + '@' + version + '.patch' + stopColor());
+}
+
+// recursively enumerate all files in the locally installed package
+function scanFiles(pkgName, src, patch)
+{
+  const files = fs.readdirSync(path.join(curDir, 'node_modules', pkgName, src));
+  files.forEach(item =>
+  {
+    const pathname = path.join(src, item);
+    const stat = fs.lstatSync(path.join(curDir, 'node_modules', pkgName, pathname));
+    if(stat.isDirectory()) scanFiles(pkgName, pathname, patch);
+    else createPatch(pkgName, pathname, patch);
+  });
+}
+
+// compare a modified file from the local package to its original counterpart - if they are different, create a patch and append it to the patch stream
+function createPatch(pkgName, pathname, patch)
+{
+  const newFile = path.join(curDir, 'node_modules', pkgName, pathname);
+  const oldFile = path.join(tmpDir, pkgName, pathname);
+  let oldStr = fs.existsSync(oldFile) ? fs.readFileSync(oldFile, 'utf8') : '';
+  let newStr = fs.readFileSync(newFile, 'utf8');
+  if(pathname === 'package.json') return; // skip "package.json" - comparison is not reliable because NPM reorders keys and also appends many system keys (whose names begin with underscore)
+  /*
+  {
+    let oldJson = {}, newJson = {};
+    try
+    {
+      oldJson = JSON.parse(oldStr);
+      newJson = JSON.parse(newStr);
+    }
+    catch(e)
+    {
+      echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'Could not parse ' + startColor('green') + 'package.json' + stopColor() + ' = ' + startColor('redBright') + e.message + stopColor());
+      return;
+    }
+    // remove all keys which start with underscore
+    let key;
+    for(key in oldJson)
+      if(key[0] === '_') delete oldJson[key];
+    for(key in newJson)
+      if(key[0] === '_') delete newJson[key];
+    // sort the keys
+    let oldSorted = {}, newSorted = {};
+    Object.keys(oldJson).sort().forEach(function(key)
+    {
+      oldSorted[key] = oldJson[key];
+    });
+    Object.keys(newJson).sort().forEach(function(key)
+    {
+      newSorted[key] = newJson[key];
+    });
+    oldStr = JSON.stringify(oldSorted, null, 2);
+    newStr = JSON.stringify(newSorted, null, 2);
+  }
+  */
+  if(oldStr !== newStr) patch.write(diff.createTwoFilesPatch(oldFile.replace(tmpDir,''), newFile.replace(path.join(curDir, 'node_modules'),''), oldStr, newStr));
+}
+
+// fetch original NPM package, then read the patch file and try to apply hunks
+function readPatch(pkgName, version)
+{
+  echo('Applying patch for: ' + startColor('magentaBright') + pkgName + stopColor());
+  const cfg = getConfig(pkgName);
+  if(cfg)
+  {
+    if(cfg.version !== version) echo(startColor('yellowBright') + 'WARNING: ' + stopColor() + 'The patch for ' + startColor('greenBright') + version + stopColor()
+      + ' may not apply cleanly to the installed ' + startColor('redBright') + cfg.version + stopColor());
+    const patch = fs.readFileSync(path.join(patchDir, pkgName + '@' + version + '.patch'),'utf8');
+    diff.applyPatches(patch,
+      {
+        loadFile: loadFile,
+        patched: onPatch,
+        complete: onComplete.bind(null, pkgName + '@' + version + '.patch')
+      });
+  }
+}
+
+function loadFile(info, callback)
+{
+  /*
+  info =
+  {
+    index: '\vue2-dragula\dist\vue-dragula.js',
+    oldFileName: '\vue2-dragula\dist\vue-dragula.js',
+    oldHeader: '',
+    newFileName: '\vue2-dragula\dist\vue-dragula.js',
+    newHeader: '',
+    hunks: [object1, object2, ...]
+  }
+   */
+  // read the original file
+  fs.readFile(path.join(curDir, 'node_modules', path.normalize(info.index)), 'utf8', function (err, data)
+  {
+    callback(err, data);
+  });
+}
+
+function onPatch(info, content, callback)
+{
+  // replace original file with the patched content
+  if(content !== false) fs.writeFile(path.join(curDir, 'node_modules', path.normalize(info.index)), content, 'utf8', function (err)
+  {
+    callback(err);
+  });
+  else
+  {
+    echo(startColor('yellowBright') + 'WARNING: ' + stopColor() + 'The patch for ' + startColor('greenBright') + path.normalize(info.index) + stopColor() + ' was not applied - '
+      + startColor('redBright') + ' either already applied or for different version' + stopColor());
+    callback();
+  }
+}
+
+function onComplete(patchName, err)
+{
+  if(err) echo(startColor('redBright') + 'ERROR: ' + stopColor() + 'The patch ' + startColor('greenBright') + patchName + stopColor() + ' produced an error = ' + startColor('redBright') + err + stopColor());
+  else echo('Successfully applied ' + startColor('greenBright') + patchName + stopColor());
+}
