0b3920ede66c1d8f23a5730511999375b471e2b8
tmcdos <tmcdos@abv.bg>
2019-06-14 22:32:58 +0300

    Trying to make the code compatible with Netlify

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
-const npmFolder = path.join(npmDir(),'node_modules','npm','node_modules');
-const pacote = require(path.join(npmFolder,'pacote'));
-const rimraf = require(path.join(npmFolder,'rimraf'));
+//const npmFolder = path.join(npmDir(),'node_modules','npm','node_modules');
+const pacote = require('pacote' /*path.join(npmFolder,'pacote')*/);
+const rimraf = require('rimraf' /*path.join(npmFolder,'rimraf')*/);
 const diff = require('diff');
 
 const version = '1.0';
